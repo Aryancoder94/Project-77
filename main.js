a1942038c5de42e7a99293360433daa53a51d456
Aryancoder94 <86225017+Aryancoder94@users.noreply.github.com>
2021-06-21 14:10:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+console.log("myfirstconsoleproject");
+
+var first_name="Aryan";
+var last_name="Chakole";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var number_1=9;
+var number_2=4;
+
+var add=number_1+number_2;
+console.log(add);
